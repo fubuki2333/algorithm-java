dfdf8cedfcdfb7714ae153de3145177aa248005c
fubuki2333 <gaochenyu2012@126.com>
2020-08-16 22:11:51 +0800

    图像渲染

--- src/com/leetcode/practise/Solution.java
+++ src/com/leetcode/practise/Solution.java
@@ -1,8 +1,6 @@
 package com.leetcode.practise;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
@@ -104,6 +102,41 @@ public class Solution {
             }
         }
         return sb.toString();
+    }
+
+
+    /**
+     * 图像渲染
+     * @param image
+     * @param sr
+     * @param sc
+     * @param newColor
+     * @return
+     */
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        int currentColor = image[sr][sc];
+        if (currentColor == newColor)
+            return image;
+
+        int[] dx = {1, 0, 0, -1};
+        int[] dy = {0, 1, -1, 0};
+
+        int n = image.length,m = image[0].length;
+        Queue<int[]> queue = new LinkedList<int[]>();
+        queue.offer(new int[]{sr,sc});
+        image[sr][sc] = newColor;
+        while (!queue.isEmpty()){
+            int[] cell = queue.poll();
+            int x = cell[0],y = cell[1];
+            for (int i = 0; i < 4; i++) {
+                int mx = x + dx[i], my = y + dy[i];
+                if (mx >= 0 && mx < n && my >= 0 && my < m && image[mx][my] == currentColor) {
+                    queue.offer(new int[]{mx, my});
+                    image[mx][my] = newColor;
+                }
+            }
+        }
+        return image;
     }
 
 }
